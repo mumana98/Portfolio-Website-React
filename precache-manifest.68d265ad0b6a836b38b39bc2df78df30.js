cbcc004af9e1a38ecc1c4752af0307b35da2d1b8
Matthew Umana <mumana98@gmail.com>
2021-01-09 21:44:15 -0600

    Updates

---
+++ precache-manifest.68d265ad0b6a836b38b39bc2df78df30.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfbc31afe2023b59bace14960d45c4c",
+    "url": "/Portfolio-Website-React/index.html"
+  },
+  {
+    "revision": "cbffdb83f7f0c439ad93",
+    "url": "/Portfolio-Website-React/static/css/main.8a4c81e3.chunk.css"
+  },
+  {
+    "revision": "bfbe50c88c763cfcd6f2",
+    "url": "/Portfolio-Website-React/static/js/2.58f0a8fe.chunk.js"
+  },
+  {
+    "revision": "89952a4296e59c57a67378493e67a155",
+    "url": "/Portfolio-Website-React/static/js/2.58f0a8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbffdb83f7f0c439ad93",
+    "url": "/Portfolio-Website-React/static/js/main.13359e3b.chunk.js"
+  },
+  {
+    "revision": "32fffa809c5bcb61ce2e",
+    "url": "/Portfolio-Website-React/static/js/runtime-main.f3062c71.js"
+  },
+  {
+    "revision": "c0bb7499d5790b1804613ad33a4aa9f2",
+    "url": "/Portfolio-Website-React/static/media/Austin.c0bb7499.png"
+  },
+  {
+    "revision": "f90a95f930e0592ee05b44fd4c07ec6d",
+    "url": "/Portfolio-Website-React/static/media/Houston.f90a95f9.png"
+  },
+  {
+    "revision": "19b25e3cc2dbd45b7d3bc6c926e67e43",
+    "url": "/Portfolio-Website-React/static/media/Resume_V2.19b25e3c.pdf"
+  },
+  {
+    "revision": "543761c6f36f5fdd844fb89ee596fc05",
+    "url": "/Portfolio-Website-React/static/media/SanAntonio.543761c6.png"
+  },
+  {
+    "revision": "f8b5e5a82188dcf7e6e5e3dcc2e3ec55",
+    "url": "/Portfolio-Website-React/static/media/ZombieHorrorProject.f8b5e5a8.png"
+  },
+  {
+    "revision": "705247c39e78c28bf0cda0f137b24cab",
+    "url": "/Portfolio-Website-React/static/media/arrow.705247c3.png"
+  },
+  {
+    "revision": "0de996fde631b3eb22deaab7dc8b6925",
+    "url": "/Portfolio-Website-React/static/media/image (4)-min.0de996fd.png"
+  },
+  {
+    "revision": "efadfa40ee44c6e06d15b8ee0d7f953c",
+    "url": "/Portfolio-Website-React/static/media/selfPortrait.efadfa40.png"
+  },
+  {
+    "revision": "7addc5c66c7fabf10544c8122aefe8ac",
+    "url": "/Portfolio-Website-React/static/media/todo.7addc5c6.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Portfolio-Website-React/precache-manifest.68d265ad0b6a836b38b39bc2df78df30.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-Website-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
