8c382196237eb450715f755dd01d18bcdfb6ddb8
Matthew Umana <mumana98@gmail.com>
2021-01-12 00:35:04 -0600

    resume is now a link to aresume to download

--- src/components/SectionThree.js
+++ src/components/SectionThree.js
@@ -13,10 +13,12 @@ const SectionThree = () => {
         data-top-bottom={w > 1100 ? "background-size: 150%" : "background-position-x: -300px"}
         >
             <h2>RESUME</h2>
-            <embed id="resume" src={resume}
-                data-bottom-top="transform:translateY(450px)"
-                data-top-bottom="transform:translateY(-200px)"
-            />
+            <a href={resume}>
+                <embed id="resume" src={resume}
+                    data-bottom-top="transform:translateY(450px)"
+                    data-top-bottom="transform:translateY(-200px)"
+                />
+            </a>
         </section> 
     )
 }
